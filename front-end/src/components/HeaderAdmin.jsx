a751e7f7aed9d8037af3ea02fc6f935bff69b870
williambrito98 <williambrito199850@gmail.com>
2022-03-30 13:47:36 -0300

    feat: add btn download zip

--- front-end/src/components/HeaderAdmin.jsx
+++ front-end/src/components/HeaderAdmin.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BASE_URL_API } from "../variables";
 
 const HeaderAdmin = () => {
     return (
         <header className="bg-primary">
-            <nav className="navbar navbar-dark ">
+            <nav className="navbar navbar-dark">
                 <Link className="navbar-brand " to="/admin">Dashboard</Link>
+                <a className="bg-light text-dark my-3 nav-item nav-link rounded" target="_blank" href={`${BASE_URL_API}/downloadZip`} rel="noreferrer">BAIXAR ZIP</a>
             </nav>
         </header>
     )
